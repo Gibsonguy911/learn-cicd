367673872610d0ca3627d300a8da1f5c7444b278
Tucker Sullivan <tuckersullivan@ymail.com>
2025-03-17 18:38:36 -0500

    Addtests (#1)
    * Update README
    
    * fix README
    
    * add basic pipleline
    
    * update basic pipeline
    
    * add bad tests
    
    * fix bad tests
    
    * add code coverage
    
    * add badge to README
    
    ---------
    
    Co-authored-by: Tucker Sullivan <Gibsonguy911@gmail.com>

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr error
+	}{
+		{
+			name:    "no auth header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:    "malformed auth header",
+			headers: http.Header{"Authorization": []string{"Bearer"}},
+			want:    "",
+			wantErr: errors.New("malformed authorization header"),
+		},
+		{
+			name:    "valid auth header",
+			headers: http.Header{"Authorization": []string{"ApiKey 123"}},
+			want:    "123",
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := GetAPIKey(tt.headers)
+			if got != tt.want {
+				t.Errorf("GetAPIKey() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
